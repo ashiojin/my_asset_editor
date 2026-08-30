4aff3eeb7e7a781ec74687ba7475640107a5ec6a
MORI, Yuji <ashiojin@gmail.com>
2026-08-30 16:14:29 +0900

    Connect mask group to api only on clip node

--- tauri-app/src/previewer/api.ts
+++ tauri-app/src/previewer/api.ts
@@ -1,6 +1,7 @@
 import { useVueFlow } from '@vue-flow/core'
 import { AdditiveBlendNodeData, BlendNodeData, ClipNodeData, MyGraphNode, RootNodeData } from '../components/nodes/NodeTypes'
 import { GraphCommand, GraphCommandType } from '../components/Command'
+import { useMaskGroupListStore } from '../stores/MaskGroupList'
 export type ApiGraph = {
     nodes: ApiNodes,
     edges: ApiEdges,
@@ -47,8 +48,30 @@ export type ApiMaskGroup = {
 
 export function getCurrentGraph(): ApiGraph {
     const { getNodes, getEdges } = useVueFlow('animation_graph')
+    const mask_group_store = useMaskGroupListStore()
 
-    let api_masks: ApiMaskGroups = [] // TODO: imprements
+    const mask_group_id_to_idx = new Map<number, number>()
+    mask_group_store.mask_group_list.forEach((item, idx) => {
+        mask_group_id_to_idx.set(item.id, idx)
+    })
+
+    function mask_ids_to_api_idxes(masks: number[]) {
+        const api_idxes: ApiMaskId[] = []
+        for (let mask_id of masks) {
+            const api_idx = mask_group_id_to_idx.get(mask_id)
+            if (api_idx === undefined) {
+                console.error('getCurrentGraph', 'mask_id not found in mask_group_list', mask_id)
+                continue
+            }
+            api_idxes.push(api_idx)
+        }
+        return api_idxes
+    }
+
+    let api_masks: ApiMaskGroups = mask_group_store.mask_group_list
+        .map((item) => {
+            return { name: item.name, targets: item.targets }
+        })
 
     let api_nodes: ApiNodes = {}
     let api_edges: ApiEdges = []
@@ -58,11 +81,12 @@ export function getCurrentGraph(): ApiGraph {
         switch (node.type) {
             case "clip":
                 const clip_data = node.data as ClipNodeData
+                const mask = mask_ids_to_api_idxes(clip_data.masks)
                 api_nodes[clip_data.label_id] = {
                     "Clip": {
                         clip: clip_data.clip_name,
                         weight: clip_data.weight,
-                        mask: [],
+                        mask,
                     }
                 }
                 map_id_to_label_id[node.id] = clip_data.label_id
